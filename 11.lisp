669b9f88c68333fef0f1f56fcd4735fd1ce7c5c2
k3ut0i <k3tu0isui@gmail.com>
2018-12-11 20:31:35 +0530

    Day11: Optimized calculations done per square. done part 2

--- 11.lisp
+++ 11.lisp
@@ -11,57 +11,62 @@
     ;;(format t "~A ~A ~A ~A~%" rack-id power-begin power-level hundredth)
     (- (floor (/ hundredth 100)) 5)))
 
-(defun make-grid (input)
-  (let ((grid (make-array (list 300
-				300)
+(defun make-grid (grid-size input)
+  (let ((grid (make-array (list grid-size
+				grid-size)
 			  :initial-element 0)))
-    (dotimes (x 300 grid)
-      (dotimes (y 300)
+    (dotimes (x grid-size grid)
+      (dotimes (y grid-size)
 	(setf (aref grid x y)
 	      (calculate-power-level x y input))))))
 
+(defun accumulate-grid (grid)
+  (destructuring-bind (xdim ydim)
+      (array-dimensions grid)
+    (dotimes (x xdim grid)
+      (dotimes (y ydim)
+	(incf (aref grid x y)
+	      (if (= x 0) 0 (aref grid (1- x) y)))))))
+
 (defun total-power (x y size grid)
   (let ((acc 0))
-    (dotimes (i size acc)
-      (dotimes (j size)
-	(incf acc (aref grid
-			(+ x i)
-			(+ y j)))))))
+    (dotimes (j size acc)
+      (incf acc (- (aref grid (+ (1- x)  size) (+ y j))
+		   (if (= x 0) 0 (aref grid (1- x) (+ y j))))))))
 
-(defun part1 (input)
-  (let* ((grid (make-grid input))
-	 (max (total-power 0 0 3 grid))
-	 (max-tl (aref grid 0 0)))
-    (dotimes (i 298 max-tl)
-      (dotimes (j 298)
-	(let ((current (total-power i j 3 grid)))
-	  (when (> current max)
-	    (setq max current
-		  max-tl (cons (1+ i)
-			       (1+ j)))))))))
-
-(defun max-power (size input)
-  (let ((grid (make-grid input))
+(defun max-power (square-size grid-size input)
+  (let ((grid (accumulate-grid (make-grid grid-size input)))
 	(max 0)
 	(max-tl (cons 0 0)))
-    (dotimes (i (- 300 (1- size)))
-      (dotimes (j (- 300 (1- size)))
-	(let ((current-power (total-power i j size grid)))
+    (dotimes (i (- grid-size (1- square-size)))
+      (dotimes (j (- grid-size (1- square-size)))
+	(let ((current-power (total-power i j square-size grid)))
 	  (when (> current-power max)
 	    (setq max current-power
 		  max-tl (list (1+ i) (1+ j)))))))
     (values max max-tl)))
 
+(defun print-grid (grid)
+  (destructuring-bind (xdim ydim)
+      (array-dimensions grid)
+    (dotimes (j ydim)
+      (dotimes (i xdim)
+	(format t "~2<~A~> " (aref grid i j)))
+      (princ #\Newline))))
+
+(defun part1 (input)
+  (max-power 3 300 input))
+
 (defun part2 (input)
-  (let ((max-tl nil)
-	(max 0)
-	(max-size 0))
-    (loop
-       :for size :from 1 :to 300
-       :do (multiple-value-bind (power tl)
-	       (max-power size input)
-	     (when (> power max)
-	       (setq max power
-		     max-tl tl
-		     max-size size)))
-       :finally (return (list max-tl size )))))
+  (loop
+     :with mpower = 0
+     :with mpoint = nil
+     :with msize = 0
+     :for s :from 1 :to 300
+     :do (multiple-value-bind (power point)
+	     (max-power s 300 input)
+	   (when (> power mpower)
+	     (setq mpower power
+		   mpoint point
+		   msize s)))
+     :finally (return (values mpower mpoint msize))))
