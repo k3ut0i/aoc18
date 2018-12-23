4a4f560ef9a510efee6cca084262575a3e4e44a3
k3ut0i <k3tu0isui@gmail.com>
2018-12-23 11:34:46 +0530

    Day23: crude implementation for part2; no answer for input

--- 23.lisp
+++ 23.lisp
@@ -32,3 +32,42 @@
   (loop :for b :in bot-list
      :count (<= (distance b sb)
 		(car (last sb)))))
+
+(defun in-range (sb bot-list)
+  (loop :for b :in bot-list
+     :when (<= (distance b sb)
+	       (car (last sb)))
+     :collect b))
+
+(defun boundries (bot-list)
+  (loop :with
+     xmin = 0 :and xmax = 0 :and
+     ymin = 0 :and ymax = 0 :and
+     zmin = 0 :and zmax = 0
+     :for b :in bot-list
+     :do (progn (when (< (car b) xmin) (setq xmin (car b)))
+		(when (> (car b) xmax) (setq xmax (car b)))
+		(when (< (cadr b) ymin) (setq ymin (cadr b)))
+		(when (> (cadr b) ymax) (setq ymax (cadr b)))
+		(when (< (caddr b) zmin) (setq zmin (caddr b)))
+		(when (> (caddr b) zmax) (setq zmax (caddr b))))
+     :finally (return (list xmin xmax ymin ymax zmin zmax))))
+
+(defun point-in-range-count (point bot-list)
+  (loop :for b :in bot-list
+     :count (<= (distance point b) (car (last b)))))
+
+(defun part2 (bot-list)
+  (destructuring-bind (xmin xmax ymin ymax zmin zmax)
+      (boundries bot-list)
+    (loop
+       :with max-val = 0 :and max-point = '(0 0 0)
+       :for x :from xmin :to xmax
+       :do (loop :for y :from ymin :to ymax
+	      :do (loop :for z :from zmin :to zmax
+		     :do (let ((d (point-in-range-count (list x y z)
+							bot-list)))
+			   (when (> d max-val)
+			     (setq max-val d)
+			     (setq max-point (list x y z))))))
+       :finally (return (list max-point max-val)))))
